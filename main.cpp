43bbd294fe9ef14fa53a83c29d5ae8fc68fe3150
marcel corso <marcel@messagebird.com>
2018-04-29 20:07:00 +0200

    the dumbest of the limiters

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool first_call = true;
 std::chrono::time_point<std::chrono::high_resolution_clock> last_call;
 
-void ratelimited() {
+void printer_limited() {
   auto now = std::chrono::high_resolution_clock::now();
   if (!first_call) {
     auto diff =
@@ -20,11 +20,31 @@ void ratelimited() {
   last_call = now;
 }
 
+class Limiter {
+
+  void (*limited)();
+
+public:
+  Limiter(void (*limited)()) { this->limited = limited; }
+
+  void call() {
+    std::this_thread::sleep_for(2s);
+    limited();
+  }
+};
+
 int main(void) {
   using namespace std::literals::chrono_literals;
-  ratelimited();
+
+  Limiter limiter(printer_limited);
+  limiter.call();
+  limiter.call();
+
+  /*
+  printer_limited();
   std::cout << std ::flush;
   std::this_thread::sleep_for(2s);
-  ratelimited();
+  printer_limited();
+  */
   return (0);
 }
